49a22cb6584795cadf78e59079bc611a98d155f6
Ibidapo Ayomide Victor <ibidapoayomide754@gmail.com>
2023-01-10 04:53:30 +0100

    Category section design

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 function App() {
   return (
    <>
-   <NavBar />
+  <Main />
    </>
   )
 }
